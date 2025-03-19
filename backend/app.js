38509a2f55a932ff1fd129474e04966873ef5baf
Ayanso Mitiku <77237684+ayayo-cpu@users.noreply.github.com>
2025-03-19 09:28:53 +0300

    Create table endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 5000;
@@ -7,12 +7,21 @@ const port = 5000;
 const dbConnection = require("./db/dbConfig");
 
 // json middleware  to extract json data
-app.use(express.json())
-
+app.use(express.json());
+// createTable
+const createDatabaseTables = require("./routes/createTables");
+app.get("/create-tables", async (req, res) => {
+  const success = await createDatabaseTables();
+  if (success) {
+    res.send("Tables created successfully!");
+  } else {
+    res.status(500).send("Error creating tables. Check the server logs.");
+  }
+});
 // user routes middleware file
 const userRoutes = require("./routes/userRoute");
 // user routes middleware
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 
 // question routes middleware file
 const questionRoute = require("./routes/questionRoute");
@@ -21,22 +30,19 @@ const questionRoute = require("./routes/questionRoute");
 const authMiddleware = require("./middleware/authMiddleware");
 
 // question routes middleware ??
-app.use("/api/question", authMiddleware ,questionRoute);
+app.use("/api/question", authMiddleware, questionRoute);
 
 // answer routes middleware ??
 
 async function start() {
   try {
-    const result = await dbConnection.execute("select 'test' ")
+    const result = await dbConnection.execute("select 'test' ");
     await app.listen(port);
     console.log("database connection established.");
-    
+
     console.log(`Server running on port ${port}`);
-    
   } catch (error) {
     console.log(error);
-    
   }
 }
-start()
-
+start();
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,7 +4,6 @@ const router = express.Router();
 //  authentication middleware
 const authMiddleware = require("../middleware/authMiddleware");
 
-
 // user controller
 const { register, login, checkUser } = require("../controller/userController");
 
@@ -15,7 +14,10 @@ router.post("/register", register);
 // login user
 router.post("/login", login);
 
+router.get("/new", (req, res) => {
+  res.send("news");
+});
 // Authentication Middleware
-router.get("/check", authMiddleware ,checkUser);
+router.get("/check", authMiddleware, checkUser);
 
 module.exports = router;
