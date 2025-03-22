4db2948c1ddb27276569235db773cae91d277f40
Ayanso Mitiku <77237684+ayayo-cpu@users.noreply.github.com>
2025-03-22 18:34:00 +0300

    Revert "task 3 Register"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx"
-import Register from "./pages/Register/Register.jsx";
 
 export const AppState = createContext();
 function App() {
@@ -43,7 +42,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer/>
 
